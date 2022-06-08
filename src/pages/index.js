e471f314080f76a9574de9d177e3d4608914f775
ireaneus <bwdave@protonmail.com>
2022-06-08 16:26:43 -0600

    weird

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 export { default as Book } from './Book';
 export { default as Books } from './Books';
 export { default as HomePage } from './HomePage';
-export { default as NumbersPage } from './NumbersPage';
 export { default as SermonsPage } from './SermonsPage';
 export { default as Layout } from './Layout';
 export { default as NoPage } from './NoPage';
